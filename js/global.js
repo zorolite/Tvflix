8e6ae2070161274574a300afd5d2b17ea25cea5b
sunil barewar <sunilvbarewar@gmail.com>
2023-07-16 16:51:19 +0530

    banner and upcoming videos section done

--- js/global.js
+++ js/global.js
@@ -21,3 +21,19 @@ const searchTogglers = document.querySelectorAll("[search-toggler]");
 addEventOnElements(searchTogglers, "click", function () {
     searchBox.classList.toggle("active");
 });
+
+
+const container = document.querySelector(".slider-control");
+// where "container" is the id of the container
+container.addEventListener("wheel", function (e) {
+    if (e.deltaY > 0) {
+        container.scrollLeft += 100;
+        e.preventDefault();
+        // prevenDefault() will help avoid worrisome 
+        // inclusion of vertical scroll 
+    }
+    else {
+        container.scrollLeft -= 100;
+        e.preventDefault();
+    }
+});
