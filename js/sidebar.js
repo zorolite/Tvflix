103e5452f80a3b4b63844671308f570993539bce
sunil barewar <sunilvbarewar@gmail.com>
2023-07-17 13:23:31 +0530

    movie list page api configuration done

--- js/sidebar.js
+++ js/sidebar.js
@@ -21,19 +21,15 @@ export function sidebar() {
     sidebarInner.classList.add("sidebar-inner");
 
     sidebarInner.innerHTML = `
-    
-        < div class="sidebar-list" >
+        <div class="sidebar-list">
             <p class="title">Genre</p>
         </div >
         <div class="sidebar-list">
             <p class="title">Language</p>
 
-            <a href="./movie-list.html" menu-close class="sidebar-link">English</a>
-            <a href="./movie-list.html" menu-close class="sidebar-link">Hindi</a>
-            <a href="./movie-list.html" menu-close class="sidebar-link">French</a>
-            <a href="./movie-list.html" menu-close class="sidebar-link">Chinese</a>
-            <a href="./movie-list.html" menu-close class="sidebar-link">Spanish</a>
-            <a href="./movie-list.html" menu-close class="sidebar-link">Japnese</a>
+            <a href="./movie-list.html" menu-close class="sidebar-link" onclick='getMovieList("with_original_language=en" ,"English")'>English</a>
+            <a href="./movie-list.html" menu-close class="sidebar-link" onclick='getMovieList("with_original_language=hi" ,"Hindi")'>Hindi</a>
+            <a href="./movie-list.html" menu-close class="sidebar-link" onclick='getMovieList("with_original_language=bn" ,"Bengali")'>Bengali</a>
         </div>
 
         <div class="sidebar-footer">
@@ -52,8 +48,8 @@ export function sidebar() {
             link.classList.add("sidebar-link");
             link.setAttribute("href", "./movie-list.html");
             link.setAttribute("menu-close", "");
-
-            // link.setAttribute("onclick", `getMovieList("with_genres=${genreId}", "${genreName}")`);
+            link.textContent = `${genreName}`
+            link.setAttribute("onclick", `getMovieList("with_genres=${genreId}", "${genreName}")`);
 
             sidebarInner.querySelectorAll(".sidebar-list")[0]
                 .appendChild(link);
@@ -79,13 +75,13 @@ export function sidebar() {
 
         const overlay = document.querySelector("[overlay]");
 
-        addEventsOnElements(sidebarTogglers, "click", function () {
+        addEventOnElements(sidebarTogglers, "click", function () {
             sidebar.classList.toggle("active");
             sidebarBtn.classList.toggle("active");
             overlay.classList.toggle("active");
         });
 
-        addEventsOnElements(sidebarClose, "click", function () {
+        addEventOnElements(sidebarClose, "click", function () {
             sidebar.classList.remove("active");
             sidebarBtn.classList.remove("active");
             overlay.classList.remove("active");
